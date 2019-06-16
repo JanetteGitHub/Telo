0a09a89acb580d4e1fdf6806ea0ee2418563760b
sbakD3 <drivecrazyloveyou@gmail.com>
2019-06-15 21:29:51 -0700

    public api

---
+++ food.backend/Models/LocalDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace food.backend.Models
+{
+    using Domain.Models;
+
+    public class LocalDataContext:DataContext
+    {
+        public System.Data.Entity.DbSet<food.common.Models.Product> Products { get; set; }
+    }
+}
